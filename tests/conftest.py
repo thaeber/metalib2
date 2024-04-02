69376696f9afc08da13807a47ecda285bd0fb8df
thaeber <40823270+thaeber@users.noreply.github.com>
2024-04-02 20:09:29 +0200

    fix: Setup of environment variables for sample test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import pytest
@@ -7,6 +8,10 @@ from metalib2 import Metadata
 @pytest.fixture(scope='function')
 def sample_data():
     """Loads sample metadata"""
+    # setup environment variable used by sample data
+    os.environ['RAW_DATA_PATH'] = '.'
+
+    # load sample data
     filename = Path(__file__).parent / 'data/2023-10-10.yaml'
     metadata = Metadata.load_yaml(filename)
     return metadata
